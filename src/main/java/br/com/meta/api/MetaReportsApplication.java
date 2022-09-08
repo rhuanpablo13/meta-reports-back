a6afcabbf3b8aa01a2a2ffdbe645a10588392a45
rhuan <rhuanpablo13@hotmail.com>
2022-09-08 12:36:46 -0300

    Iniciando projeto base

---
+++ src/main/java/br/com/meta/api/MetaReportsApplication.java
@@ -0,0 +1,13 @@
+package br.com.meta.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meta/api/MetaReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meta.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class MetaReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
